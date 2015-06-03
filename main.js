537d0f1bfe5aa08c60ae6cc188f58097ad32d56f
Chad Miller <wick42@gmail.com>
2015-06-02 20:45:24 -0700

    Message recieved, over and out.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 alert("I'm working with JavaScript");
+
+// var player = prompt("What is your name?")
+// var adj = prompt("Please type an adjective.");
+// var color = prompt("Please pick a color.");
+// var animal = prompt("What is your favorite animal?");
+
+function madLibs() {
+	
+	document.write("<h4>Jack and Jill went up the hill to fetch a pail of water. Jack fell down and broke his crown, and Jill came tumbling after.</h4>");
+	// document.write("<h4>Thanks for playing " + player + "!</h4>");
+
+}
+
+madLibs();
